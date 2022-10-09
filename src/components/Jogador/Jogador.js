a1d5651e681e447d0c4a9b90807b7baf85d7295e
gabriel natan <g.souza@grouplinknetwork.com>
2022-10-09 10:31:46 -0300

    create jogador

---
+++ src/components/Jogador/Jogador.js
@@ -0,0 +1,17 @@
+import "./jogador.css"
+
+function Jogador(){
+        return(
+            <div className='colaborador'>
+                <div className='cabecalho'>
+                    <img src="https://github.com/gabrielnatan.png" alt="avatar"/>
+                </div>
+                <div className='rodape'>
+                    <h1>Gabriel Natan</h1>
+                    <h5>Desenvolvedor</h5>
+                </div>
+            </div>
+        )
+}
+
+export default Jogador
--- src/components/Time/Time.js
+++ src/components/Time/Time.js
@@ -1,9 +1,11 @@
+import Jogador from "../Jogador/Jogador"
 import "./time.css"
 
 function Time({titulo,primaryColor,secondaryColor}){
     return(
         <section style={{background:primaryColor}} key={titulo} className="time">
             <h3 style={{borderColor:secondaryColor}}>{titulo}</h3>
+            <Jogador/>
         </section>
     )
 }
